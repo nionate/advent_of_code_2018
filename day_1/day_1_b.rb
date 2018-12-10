4134962c9bf5ab5da8f86955dcff756526d66080
Nicolás Oñate <nonate@thoughtworks.com>
2018-12-10 20:32:45 -0300

    Adds day 3 input

--- day_1/day_1_b.rb
+++ day_1/day_1_b.rb
@@ -1,19 +1,20 @@
-def sum(acc, num) 
-    acc + num
+def repeated_frequency
+    frequencies = {}
+    current_frequency = 0
+    file = File.open('input.txt')
+
+    file.each do |line| 
+        current_frequency += line.to_i
+        if frequencies[current_frequency].nil?
+            frequencies[current_frequency] = 1
+        else
+            return current_frequency
+        end
+    
+        if file.eof?
+            file.rewind
+        end
+    end
 end
 
-frequencies = [0]
-num = 0
-File.open('input.txt').each { |line| 
-    num = sum(num, line.to_i)
-    twice = frequencies.index(num)
-    if(twice.nil?) then
-        frequencies.push(num)
-    else
-        puts "num #{num}"
-        puts "frequencies #{frequencies}"
-        break
-    end
-}
-
-puts num
+puts repeated_frequency
