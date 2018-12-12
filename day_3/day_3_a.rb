69592ea53b0c3270bd15ae851af3e812227b592c
nonate-tw <nonate@thoughtworks.com>
2018-12-11 23:20:57 -0300

    day 3 part 1 partial solution

--- day_3/day_3_a.rb
+++ day_3/day_3_a.rb
@@ -1,37 +1,28 @@
 file = File.open('input.txt')
 re = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/m
 
-arr = Array.new(1000){Array.new(1000)}
-
-def create_fabric(array)
-    array.each_with_index { |v,i|
-        array[i].each_with_index { |d,j|
-            array[i][j] = 0
-        }   
-    }
-end
+arr = Array.new(1000){Array.new(1000, 0)}
 
 def print_fabric(array)
+    file = "output.txt"
     array.each_with_index { |v,i|
         array[i].each_with_index { |d,j|
-            print array[i][j]
+            File.write(file, array[i][j], mode:'a')
         } 
-        print  "\n"
     }
 end
 
-create_fabric arr
+puts "prueba #{arr[977][19]}"
 
 inches = 0
 file.each do |line|
     id,left_inches,top_inches,wide_inches,tall_inches = line.match(re).captures
-    puts id
-    for i in 0..tall_inches.to_i
-        for j in 0..wide_inches.to_i 
-            if arr[top_inches.to_i+i][left_inches.to_i+j] != 0 then
+    (top_inches.to_i..top_inches.to_i + tall_inches.to_i - 1).each do |i|
+        (left_inches.to_i..left_inches.to_i + wide_inches.to_i - 1).each do |j|
+            if arr[i][j].to_i != 0 then
                 inches += 1
             else
-                arr[top_inches.to_i+i][left_inches.to_i+j] = id
+                arr[i][j] = id
             end
         end
     end
