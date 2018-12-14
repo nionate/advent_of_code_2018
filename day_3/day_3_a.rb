5c607e7ab8b5dce6a8cb7eb8aaa39c41f2f32fb1
nonate-tw <nonate@thoughtworks.com>
2018-12-13 22:10:16 -0300

    solves day 3 part 1

--- day_3/day_3_a.rb
+++ day_3/day_3_a.rb
@@ -1,31 +1,23 @@
 file = File.open('input.txt')
-re = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/m
+regex = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/m
 
 arr = Array.new(1000){Array.new(1000, 0)}
 
-def print_fabric(array)
-    file = "output.txt"
-    array.each_with_index { |v,i|
-        array[i].each_with_index { |d,j|
-            File.write(file, array[i][j], mode:'a')
-        } 
-    }
-end
-
-puts "prueba #{arr[977][19]}"
-
-inches = 0
+#inches = 0
 file.each do |line|
-    id,left_inches,top_inches,wide_inches,tall_inches = line.match(re).captures
+    id,left_inches,top_inches,wide_inches,tall_inches = line.match(regex).captures
     (top_inches.to_i..top_inches.to_i + tall_inches.to_i - 1).each do |i|
         (left_inches.to_i..left_inches.to_i + wide_inches.to_i - 1).each do |j|
-            if arr[i][j].to_i != 0 then
-                inches += 1
-            else
-                arr[i][j] = id
-            end
+            #if arr[i][j].to_i != 0 then
+            #    inches += 1
+            #else
+            #    arr[i][j] = id
+            #end
+            arr[i][j] += 1
         end
     end
 end
 
-puts inches
+puts arr.flatten.count { |inches|
+    inches >= 2
+}
