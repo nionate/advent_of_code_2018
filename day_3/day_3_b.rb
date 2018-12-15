951f3bc2d2000e64cbec5716cbd966fc6fc7b828
nonate-tw <nonate@thoughtworks.com>
2018-12-15 00:16:29 -0300

    day 3 part 2 partial solution

--- day_3/day_3_b.rb
+++ day_3/day_3_b.rb
@@ -1,23 +1,48 @@
 file = File.open('input.txt')
-regex = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/m
+$regex = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/m
 
-arr = Array.new(1000){Array.new(1000, 0)}
-ids = Array.new(1000){Array.new(1000, 0)}
+arr = Array.new(1000){Array.new(1000, [])}
+
+def claims(file)
+    claims = []
+    file.each do |line|
+        id,left_inches,top_inches,wide_inches,tall_inches = line.match($regex).captures  
+        claims << id
+    end
+    return claims
+end
+
+valid_claims = claims(file)
+
+file.rewind
 
 file.each do |line|
-    id,left_inches,top_inches,wide_inches,tall_inches = line.match(regex).captures  
+    id,left_inches,top_inches,wide_inches,tall_inches = line.match($regex).captures  
     (top_inches.to_i..top_inches.to_i + tall_inches.to_i - 1).each do |i|
         (left_inches.to_i..left_inches.to_i + wide_inches.to_i - 1).each do |j|
-            arr[i][j] += 1
-            ids[i][j] = id
+            
+            if(!arr[i][j].include? id)
+                arr[i][j] << id
+            end
+
+            if arr[i][j].length > 1 then
+                arr[i][j].each do |val|
+                    index = valid_claims.index(val)
+                    valid_claims.delete_at(index) if !index.nil?
+                end
+            end
         end
     end
 end
 
-index_claim = arr.flatten.index { |inches|
-    inches == 1
-}
+#index_claim = arr.flatten.index { |inches|
+#    inches == 1
+#}
 
-puts arr.flatten[index_claim]
+#for i in 0..arr.length-1
+#    for j in 0..arr[i].length-1
+#        puts arr[i][j] if arr[i][j].length == 1
+#    end
+#end
 
-puts ids.flatten
+puts valid_claims
